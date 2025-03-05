998c96a901d9f50040e98d4364bbf1c2d7e76d1e
zusture <danunai1993@gmail.com>
2025-03-05 20:10:11 +0700

    fix promise params lang

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 // app/[locale]/page.tsx
 import HomePage from '@/components/HomePage';
 
-export default async function Page({ params }: { params: { locale: string } }) {
+export default async function Page({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params;
   return <HomePage locale={locale} />;
 }
